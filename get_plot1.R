49a435286f892b614c479822fa68cde60b3473cc
Jorge Jose Rodriguez <borgesconsulting@gmail.com>
2017-01-08 23:02:58 -0500

    Get Plot files and graphics in .png

---
+++ get_plot1.R
@@ -0,0 +1,9 @@
+getfile <- file("household_power_consumption.txt")
+get_data <- read.table(text = grep("^[1,2]/2/2007", readLines(getfile), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+
+# Generating Plot 1
+hist(get_data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+
+png(filename="plot1.png")
+hist(get_data$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+dev.off()
